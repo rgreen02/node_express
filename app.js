4abfec2f56889e3a8a48b35abd8bfdc724204853
Bob Green <bob.green@madysoft.com>
2020-06-12 22:42:38 -0400

    Session 2

--- app.js
+++ app.js
@@ -48,37 +48,22 @@ app.use(
   }),
 );
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   //console.log(req.headers);
   //if (!req.signedCookies.user) {
   if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-    const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === "admin" && pass === "password") {
-      //res.cookie("user", "admin", { signed: true });
-      req.session.user = "admin";
-      return next(); // authorized
-    } else {
-      const err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
+    const err = new Error("You are not authenticated!");
+    //res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
   } else {
     //if (req.signedCookies.user === "admin") {
-    if (req.session.user === "admin") {
+    if (req.session.user === "authenticated") {
       return next();
     } else {
       const err = new Error("You are not authenticated!");
@@ -88,11 +73,8 @@ function auth(req, res, next) {
   }
 }
 app.use(auth);
-
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
