dfc439ee494b7cc4ca0fe77b436c0798dc1af05f
Bob Green <bob.green@madysoft.com>
2020-06-12 23:00:49 -0400

    Passport

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const FilesStore = require("session-file-store")(session);
+const passport = require("passport");
+const authenticate = require("./authenticate");
 
 //Routing Middleware
 const indexRouter = require("./routes/index");
@@ -47,29 +49,21 @@ app.use(
     store: new FilesStore(),
   }),
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  //console.log(req.headers);
-  //if (!req.signedCookies.user) {
-  if (!req.session.user) {
+  if (!req.user) {
     const err = new Error("You are not authenticated!");
-    //res.setHeader("WWW-Authenticate", "Basic");
     err.status = 401;
     return next(err);
   } else {
-    //if (req.signedCookies.user === "admin") {
-    if (req.session.user === "authenticated") {
-      return next();
-    } else {
-      const err = new Error("You are not authenticated!");
-      err.status = 401;
-      return next(err);
-    }
+    return next();
   }
 }
 app.use(auth);
